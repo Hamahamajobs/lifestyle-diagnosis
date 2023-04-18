2118d7a432563ebb2642aac42bf7c9502ca4587c
Kazutaka Hamada <hama0102105@gmail.com>
2023-04-18 23:10:29 +0900

    nuxt導入

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -1,4 +1,18 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  vite: {
+    define: {
+      'process.env.DEBUG': false,
+    }
+  },
+  css: ['vuetify/lib/styles/main.sass', 'mdi/css/materialdesignicons.min.css'],
+  build: {
+    transpile: ['vuetify'],
+  },
+  app: {
+    head: {
+      title: 'test',
+      meta: [{ name: 'description', content: 'test' }],
+    },
+  },
 
 })
