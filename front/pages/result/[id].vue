917d11513c580bdc1a371389b0d008c5cd3dddd0
Kazutaka Hamada <hama0102105@gmail.com>
2023-07-08 12:39:17 +0900

    add: 回答結果画面の骨組み

--- front/pages/result/[id].vue
+++ front/pages/result/[id].vue
@@ -19,10 +19,61 @@ const resultType = computed(() => {
 <template>
   <div>
     <div class="lp-container d-flex justify-center align-center flex-column">
-      <!-- main -->
-      <div class="mb-5">
-        <p>あなたの診断結果</p>
+      <!-- 一番上のサマリー的なところ -->
+      <div class="top-container mb-5">
+        <div class="">
+          診断結果
+        </div>
+        <div class="top-main d-flex">
+          <div class="left d-flex flex-column">
+            <p>達観型</p>
+            <p>精神的に安定しており、頼られることが好き</p>
+            <p>精神的に安定しており、どんなときも基本的に冷静に対応できる。そのため、周囲の人からの信頼が厚く、相談に乗ることも得意。</p>
+          </div>
+          <div class="right">
+            画像
+          </div>
+        </div>
       </div>
+      <!-- シェアブロック -->
+      <div class="share-container mb-5">
+        <p>#share your life</p>
+        <p>“あなたの生き方”を伝える</p>
+        <p>■ ■ ■</p>
+      </div>
+
+      <!-- トリセツ情報 ブロック -->
+      <!-- TODO: コンポーネント化 -->
+      <div class="share-container mb-5">
+        <p>あなたの、特徴は？</p>
+        <p>物怖じせず、どっしり構える</p>
+        <p>物怖じせずどっしり構える物怖じせずどっしり構える物怖じせずどっしり構える物怖じせずどっしり構える物怖じせずどっしり構える物怖じせずどっしり構える物怖じせずどっしり構える物怖じせずどっしり構える</p>
+      </div>
+
+      <!-- サマリーブロック -->
+      <!-- TODO: コンポーネント化 -->
+      <div class="mb-5">
+        <p>
+          サマリーのブロック/p>
+        </p>
+      </div>
+
+      <!-- サービス説明 ブロック -->
+      <!-- TODO: コンポーネント化 -->
+      <div class="mb-5">
+        <p>
+          サービス説明のブロック/p>
+        </p>
+      </div>
+
+      <!-- 13のタイプ一覧を見る ブロック -->
+      <!-- TODO: コンポーネント化 -->
+      <div class="mb-5">
+        <p>
+          13のタイプ一覧を見るのブロック/p>
+        </p>
+      </div>
+
       <div class="mb-5">
         <p>{{ resultType.jp }}</p>
       </div>
