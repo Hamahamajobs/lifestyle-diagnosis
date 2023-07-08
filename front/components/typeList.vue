ae44b1dd961e1af17a710078bf134e203dc5779b
bro shaQ <tkm.kwsm@gmail.com>
2023-07-08 14:05:27 +0900

    13タイプリスト一旦コミット

---
+++ front/components/typeList.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <div class="d-flex type-container">
+      <div v-for="n in 13" :key="n">
+        <div class="type-item">
+          hoggehoge
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+  .type {
+    &-container {
+      width: 80vw;
+      overflow: hidden;
+    }
+    &-item {
+      width: 400px;
+    }
+  }
+</style>
