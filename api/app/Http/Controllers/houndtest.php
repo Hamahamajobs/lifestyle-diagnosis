dace274bd0be0ce6bf7da6a67aeb3c686f52ff10
Kazutaka Hamada <hama0102105@gmail.com>
2023-09-27 07:13:40 +0900

    hound test2

---
+++ api/app/Http/Controllers/houndtest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\UseCase\InsertAnswer;
+
+use App\Exceptions\UseCaseExceptions\BadRequestException;
+
+class AnswerController extends Controller
+{
+    /**
+     * 設問取得
+     * 
+     * @param Request $request リクエストパラメータ
+     * @param GetQuestions $getQuestions ユースケースクラス
+     * @return object レスポンスJSON
+     */
+    public function store(Request $request, InsertAnswer $useCase)
+    {
+
+        try {
+            $result = $useCase->invoke(
+                $request->answers
+            );
+
+            return response()->json([
+                'result' => $result
+            ], 200);
+        } catch (BadRequestException $e) {
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 400);
+        }
+    }
+}
