a5eb441de288a789f62166d6ceaa5cf010608ab4
Kazutaka Hamada <hama0102105@gmail.com>
2023-11-18 10:38:50 +0900

    update2

--- api/app/Services/Rabbit.php
+++ api/app/Services/Rabbit.php
@@ -31,7 +31,27 @@ class Rabbit
 
   public function call4()
   {
-    // ログを出しません
-    Log::info('rabbit', ['rabbit1' => 'rabbit']);
+    // 悪い例: 不要な変数の使用
+    $a = 1;
+    $b = 1;
+    $sum = $a + $b;
+    $average = $sum / 2;
+    echo "Average: " . $average;
   }
+
+  public function performTask($data) {
+    // 悪い例: 重複するコード
+    $result1 = $this->addNumbers($data);
+    // 処理...
+    $result2 = $this->addNumbers($data);
+  }
+
+  // 悪い例: 過度なコメント
+  private function addNumbers($a, $b)
+  {
+    // 2つの数を足して結果を返す
+    $result = $a + $b;
+    return $result;
+  }
+  
 }
