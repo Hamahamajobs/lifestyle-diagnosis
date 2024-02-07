6751ed83991b23ae0876569e50e51126c5b29dc4
Kazutaka Hamada <hama0102105@gmail.com>
2024-02-07 16:30:34 +0900

    coderabbit確認用コード追加

---
+++ api/app/Http/UseCase/GetQuestions.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\UseCase;
+
+// use App\Models\Answer;
+use App\Models\Question;
+use App\Models\Category;
+use App\Models\PersonalType;
+
+
+class GetQuestions {
+    /**
+     * 設問取得
+     */
+   public function invoke(){
+     $questons = Question::from('questions as q')->leftJoin('categories as c', 'q.category_id', '=', 'c.id')->select(['q.id', 'c.category_name','q.question_contents','is_reversal'])->orderBy('q.id', 'asc')->get();
+    
+     return $questons;
+   }
+}
